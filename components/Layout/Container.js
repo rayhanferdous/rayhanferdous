4127a8ccfb7ad6e6d0c59fc0c62245963fcc4381
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-15 21:13:36 +0600

    Create Container.js

---
+++ components/Layout/Container.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Container({ children }) {
+  return (
+    <div className="w-full px-9 xl:px-0 xl:w-1440 xl:mx-auto flex flex-col">
+      {children}
+    </div>
+  );
+}
+
+export default Container;
