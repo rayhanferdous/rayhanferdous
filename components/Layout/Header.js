f1a644919f69469434f873acea81f6f5e7469594
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-16 08:38:18 +0600

    Create Header.js

---
+++ components/Layout/Header.js
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import NavMenuCard from "./NavMenuCard";
+import Image from "next/image";
+import HamBurger from "../Svg/HamBurger";
+
+function Header({ setIsOpen }) {
+  return (
+    <>
+      <header className="flex items-center justify-between mt-7 z-10">
+        <Image
+          src={"/assets/logo.png"}
+          width={70}
+          height={32}
+          alt="rayhan-ferdous"
+        />
+        <NavMenuCard customStyle={"max-lg:hidden"} />
+        <div onClick={setIsOpen} className="lg:hidden">
+          <HamBurger />
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
