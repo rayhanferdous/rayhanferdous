0108e03a9eea5350d4fb954d5beda2ced833954d
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-16 11:07:34 +0600

    Create ProjectShowcase.js

---
+++ components/Layout/Index/ProjectShowcase.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Rectangle from "../Rectangle";
+import ProjectShowcaseCard from "./ProjectShowcaseCard";
+import { projectShowcaseData } from "@/data/ProjectShowcaseData";
+
+function ProjectShowcase() {
+  return (
+    <div className="relative">
+      <div className="mb-12">
+        <h2 className="text-projectsTitle font-bold text-[56px] mb-4">
+          My Projects
+        </h2>
+        <p className="text-secondery text-lg">
+          Here is my some project. I am trying to somthing is better then other
+          websites and <br /> developers. Thats why I used modern technology in
+          those websites.
+        </p>
+      </div>
+      <div className="grid gap-16 grid-cols-1 md:grid-cols-2 ">
+        {projectShowcaseData.slice({ length: 4 }).map((item, index) => (
+          <ProjectShowcaseCard
+            key={index}
+            id={item.id}
+            title={item.title}
+            description={item.description}
+            image={item.image}
+            stack={item.stack}
+            livepreview={item.livepreview}
+            get_code={item.get_code}
+          />
+        ))}
+      </div>
+      <div className="flex items-center justify-center">
+        <button className="w-[138px] py-3 mt-[58px] rounded-[7px] border-2 border-solid border-btnPrimary text-btnPrimary font-medium text-lg">
+          See More
+        </button>
+      </div>
+      <Rectangle className={"top-[25%] -right-[55%]"} />
+    </div>
+  );
+}
+
+export default ProjectShowcase;
