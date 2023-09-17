4e430e33bec5c6a6baab8c8147fa98fe69c53355
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-17 18:49:29 +0600

    Create NavMenuCard.js

---
+++ components/Layout/NavMenuCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function NavMenuCard({ customStyle }) {
+  const NavLinks = [
+    "{ Home }",
+    ".About Me{}",
+    ".Experience{}",
+    ".My Projects{}",
+  ];
+  return (
+    <div className={`flex gap-12 max-lg:flex-col ${customStyle}`}>
+      <div className="flex items-center gap-30 max-lg:flex-col">
+        {NavLinks.map((item, index) => (
+          <div
+            key={index}
+            className={`${
+              item == "{ Home }" ? "text-btnPrimary" : "text-secondery"
+            } flex items-center  text-lg leading-5`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <button className="bg-btnPrimary px-7 py-3 rounded-5px text-white  text-lg leading-5">
+        {".Hire Me{}"}
+      </button>
+    </div>
+  );
+}
+
+export default NavMenuCard;
