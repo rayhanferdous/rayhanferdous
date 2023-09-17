45a74e51bb862ab2d6c2b586e8f8b54c44744def
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-17 18:49:46 +0600

    Delete Container.jsx

--- components/Layouts/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Container = ({children}) => {
-  return (
-    <div className='w-full max-xl:px-2.5 xl:w-[1260px] xl:mx-auto'>{children}</div>
-  )
-}
-
-export default Container
