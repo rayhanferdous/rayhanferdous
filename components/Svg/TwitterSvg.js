457e749f239035743cdd58b09c0861de252d252f
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-18 22:43:22 +0600

    Update TwitterSvg.js

--- components/Svg/TwitterSvg.js
+++ components/Svg/TwitterSvg.js
@@ -5,6 +5,7 @@ export default function TwitterSvg() {
     <svg
       width="25"
       height="25"
+      className="cursor-pointer"
       viewBox="0 0 25 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
