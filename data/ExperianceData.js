3b8094586ad6fc50bda1d1ff1b77665b44e90c46
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-19 07:01:52 +0600

    Create ExperianceData.js

---
+++ data/ExperianceData.js
@@ -0,0 +1,18 @@
+export const experianceData = [
+  {
+    id: 1,
+    position: "Junior Developer",
+    company: "IRD Foundation, Banasree, Dhaka",
+    duration: "2022 - Present",
+    details:
+      "Configure & deploy web applications based on the LEMP and LAMP stacks.Troubleshoot various production environment issues. Training new candidates on various system administration processes",
+  },
+  {
+    id: 2,
+    position: "Junior Developer",
+    company: "IRD Foundation, Banasree, Dhaka",
+    duration: "2022 - Present",
+    details:
+      "Configure & deploy web applications based on the LEMP and LAMP stacks.Troubleshoot various production environment issues. Training new candidates on various system administration processes",
+  },
+];
