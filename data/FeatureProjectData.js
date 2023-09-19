778e92e754ed510b635eaf3e6596dea0d6aadec6
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-19 07:01:54 +0600

    Create FeatureProjectData.js

---
+++ data/FeatureProjectData.js
@@ -0,0 +1,18 @@
+export const featureProjectData = [
+  {
+    id: 1,
+    title: "QuranMazid",
+    description: "QuranMazid.com",
+    image: "/assets/projects/quranmazid.com.png",
+    technologies: ["Next.Js", "Laravel", "Redux", "Tailwind CSS", "More"],
+    live_preview: "https://ihadis.com/",
+  },
+  {
+    id: 2,
+    title: "DuaRuqyah",
+    description: "QuranMazid.com",
+    image: "/assets/projects/duaruqyah.com.png",
+    technologies: ["Next.Js", "Laravel", "Redux", "Tailwind CSS", "More"],
+    live_preview: "https://ihadis.com/",
+  },
+];
