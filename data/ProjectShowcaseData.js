24c9c72d6bf4feaf9d4dc04e2208239ab32475f6
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-19 20:03:37 +0600

    Create ProjectShowcaseData.js

---
+++ data/ProjectShowcaseData.js
@@ -0,0 +1,108 @@
+export const projectShowcaseData = [
+  {
+    id: 1,
+    title: "iHadis",
+    description:
+      "Built the client-side for general & admin users using ReactJS, React Hook Form, React Router, Bootstrap & Material UI and deployed on Firebase. Used NodeJS and ExpressJS to create personalized API on the server-side.",
+    image: "/assets/projects/ihadis.com.png",
+    stack: [
+      "/assets/hero/stack/image 3.png",
+      "/assets/hero/stack/image 4.png",
+      "/assets/hero/stack/image 5.png",
+      "/assets/hero/stack/image 6.png",
+    ],
+    livepreview: "https://ihadis.com/",
+    get_code: "",
+  },
+
+  {
+    id: 2,
+    title: "QuranMazid",
+    description:
+      "Built the client-side for general & admin users using ReactJS, React Hook Form, React Router, Bootstrap & Material UI and deployed on Firebase. Used NodeJS and ExpressJS to create personalized API on the server-side.",
+    image: "/assets/projects/quranmazid.com.png",
+    stack: [
+      "/assets/hero/stack/image 3.png",
+      "/assets/hero/stack/image 4.png",
+      "/assets/hero/stack/image 5.png",
+      "/assets/hero/stack/image 6.png",
+    ],
+    livepreview: "https://quranmazid.com/",
+    get_code: "",
+  },
+  {
+    id: 3,
+    title: "Lixzo",
+    description:
+      "Built the client-side for general & admin users using ReactJS, React Hook Form, React Router, Bootstrap & Material UI and deployed on Firebase. Used NodeJS and ExpressJS to create personalized API on the server-side.",
+    image: "/assets/projects/lixzo.com.png",
+    stack: [
+      "/assets/hero/stack/image 3.png",
+      "/assets/hero/stack/image 4.png",
+      "/assets/hero/stack/image 5.png",
+      "/assets/hero/stack/image 6.png",
+    ],
+    livepreview: "https://lixzo.com/",
+    get_code: "",
+  },
+  {
+    id: 4,
+    title: "Cuore Tours",
+    description:
+      "Buy your Panoramic Bus tickets and explore the city. The best one-day guided tours to dive into the history of Rome with Colosseum and Vatican, Pompeii, Capri and more.",
+    image: "/assets/projects/cuoretours.com.png",
+    stack: [
+      "/assets/hero/stack/image 3.png",
+      "/assets/hero/stack/image 4.png",
+      "/assets/hero/stack/image 5.png",
+      "/assets/hero/stack/image 6.png",
+    ],
+    livepreview: "https://cleaners-world.web.app/",
+    get_code: "",
+  },
+  {
+    id: 5,
+    title: "Zenith",
+    description:
+      "Built the client-side for general & admin users using ReactJS, React Hook Form, React Router, Bootstrap & Material UI and deployed on Firebase. Used NodeJS and ExpressJS to create personalized API on the server-side.",
+    image: "/assets/projects/zenith-seven.vercel.app.png",
+    stack: [
+      "/assets/hero/stack/image 3.png",
+      "/assets/hero/stack/image 4.png",
+      "/assets/hero/stack/image 5.png",
+      "/assets/hero/stack/image 6.png",
+    ],
+    livepreview: "https://zenith-seven.vercel.app/",
+    get_code: "",
+  },
+  {
+    id: 6,
+    title: "Thaw UI",
+    description:
+      "Built the client-side for general & admin users using ReactJS, React Hook Form, React Router, Bootstrap & Material UI and deployed on Firebase. Used NodeJS and ExpressJS to create personalized API on the server-side.",
+    image: "/assets/projects/thaw-ui-sigma.vercel.app.png",
+    stack: [
+      "/assets/hero/stack/image 3.png",
+      "/assets/hero/stack/image 4.png",
+      "/assets/hero/stack/image 5.png",
+      "/assets/hero/stack/image 6.png",
+    ],
+    livepreview: "https://thaw-ui-sigma.vercel.app/",
+    get_code: "",
+  },
+  {
+    id: 7,
+    title: "Knowleto",
+    description:
+      "Built the client-side for general & admin users using ReactJS, React Hook Form, React Router, Bootstrap & Material UI and deployed on Firebase. Used NodeJS and ExpressJS to create personalized API on the server-side.",
+    image: "/assets/projects/knowleto.vercel.app.png",
+    stack: [
+      "/assets/hero/stack/image 3.png",
+      "/assets/hero/stack/image 4.png",
+      "/assets/hero/stack/image 5.png",
+      "/assets/hero/stack/image 6.png",
+    ],
+    livepreview: "https://knowleto.vercel.app/",
+    get_code: "",
+  },
+];
