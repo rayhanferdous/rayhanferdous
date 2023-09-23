232505abdf5885b27fb1d6ebea61471827787945
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-23 18:48:28 +0600

    Update Container.js

--- components/Layout/Container.js
+++ components/Layout/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Container({ children }) {
   return (
-    <div className="w-full px-9 xl:px-0 xl:w-1440 xl:mx-auto flex flex-col">
+    <div className="w-full px-3 md:px-9 xl:px-0 xl:w-1440 xl:mx-auto flex flex-col">
       {children}
     </div>
   );
