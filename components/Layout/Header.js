23a9328cb9221e13c98060ef5aec71723104839a
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-23 21:23:28 +0600

    Update Header.js

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,23 +3,26 @@ import React from "react";
 import NavMenuCard from "./NavMenuCard";
 import Image from "next/image";
 import HamBurger from "../Svg/HamBurger";
+import Container from "./Container";
 
 function Header({ setIsOpen }) {
   return (
-    <>
-      <header className="flex items-center justify-between pt-7 z-10 sticky top-0">
-        <Image
-          src={"/assets/logo.png"}
-          width={70}
-          height={32}
-          alt="rayhan-ferdous"
-        />
-        <NavMenuCard customStyle={"max-lg:hidden"} />
-        <div onClick={setIsOpen} className="lg:hidden">
-          <HamBurger />
-        </div>
-      </header>
-    </>
+    <div className=" bg-primary/90 z-20 sticky top-0 py-2">
+      <Container>
+        <header className="flex items-center justify-between">
+          <Image
+            src={"/assets/logo.png"}
+            width={70}
+            height={32}
+            alt="rayhan-ferdous"
+          />
+          <NavMenuCard customStyle={"max-lg:hidden"} />
+          <div onClick={setIsOpen} className="lg:hidden">
+            <HamBurger />
+          </div>
+        </header>
+      </Container>
+    </div>
   );
 }
 
