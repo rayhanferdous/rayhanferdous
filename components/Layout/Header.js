4be19dbec995d58bea9f1452370efa5bae35d726
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-23 21:31:34 +0600

    Update Header.js

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -7,13 +7,13 @@ import Container from "./Container";
 
 function Header({ setIsOpen }) {
   return (
-    <div className=" bg-primary/90 z-20 sticky top-0 py-2">
+    <div className="backdrop-blur-sm bg-primary/80 z-20 sticky top-0 py-2">
       <Container>
         <header className="flex items-center justify-between">
           <Image
             src={"/assets/logo.png"}
-            width={70}
-            height={32}
+            width={65}
+            height={36}
             alt="rayhan-ferdous"
           />
           <NavMenuCard customStyle={"max-lg:hidden"} />
