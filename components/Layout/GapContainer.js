55fdcb1524269f11432e223e8b5454627322326d
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-30 20:19:03 +0600

    Update GapContainer.js

--- components/Layout/GapContainer.js
+++ components/Layout/GapContainer.js
@@ -1,7 +1,7 @@
 import React, { Children } from "react";
 
 function GapContainer({ children }) {
-  return <div className="flex flex-col mt-40 gap-24">{children}</div>;
+  return <div className="flex flex-col mt-6 xl:mt-40 gap-6 xl:gap-24">{children}</div>;
 }
 
 export default GapContainer;
