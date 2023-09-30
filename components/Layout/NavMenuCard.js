1a528863ee675d4de3e8fd55c2d9df59f34bbb5e
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-30 21:10:49 +0600

    Update NavMenuCard.js

--- components/Layout/NavMenuCard.js
+++ components/Layout/NavMenuCard.js
@@ -1,18 +1,42 @@
-import React from "react";
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
 
 function NavMenuCard({ customStyle }) {
-  const NavLinks = ["{Home}", ".About Me{}", ".Experience{}", ".My Projects{}"];
+  // const NavLinks = ["{Home}", ".About Me{}", ".Experience{}", ".My Projects{}"];
+  const NavLinks = [
+    { id: 1, name: "{Home}", path: "/", uniqueId: "home" },
+    // { id: 2, name: ".About Me{}", path: "/about", uniqueId: "about" },
+    {
+      id: 3,
+      name: ".Experience{}",
+      path: "/experience",
+      uniqueId: "experience",
+    },
+    { id: 4, name: ".My Projects{}", path: "/projects", uniqueId: "projects" },
+  ];
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  useEffect(() => {
+    const section = searchParams.get("section");
+    if (section) {
+      const element = document.getElementById(section);
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [searchParams]);
+
   return (
     <div className={`flex gap-12 max-lg:flex-col ${customStyle}`}>
       <div className="flex items-center gap-30 max-lg:flex-col">
         {NavLinks.map((item, index) => (
           <div
             key={index}
+            onClick={() => router.push(`/?section=${item.uniqueId}`)}
             className={`${
               item == "{ Home }" ? "text-btnPrimary" : "text-secondery"
             } flex items-center text-lg leading-5 cursor-pointer`}
           >
-            {item}
+            {item.name}
           </div>
         ))}
       </div>
