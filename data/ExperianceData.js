96c2d797f004daca2e3c79f79e7ab90efda86df4
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-30 20:28:50 +0600

    Update ExperianceData.js

--- data/ExperianceData.js
+++ data/ExperianceData.js
@@ -4,15 +4,14 @@ export const experianceData = [
     position: "Junior Developer",
     company: "IRD Foundation, Banasree, Dhaka",
     duration: "2022 - Present",
-    details:
-      "Configure & deploy web applications based on the LEMP and LAMP stacks.Troubleshoot various production environment issues. Training new candidates on various system administration processes",
+    details: `● Built responsive websites using semantic HTML5, PHP, Laravel, JavaScript, ReactJS, NextJs,and CSS, Tailwind CSS, Redux, and other tools. ● Created API, Databases where needed and connected it with frontend. ● Developed code that deals with large data sets by rendering components on UI and optimizing calls to minimize HTTP requests by 80%. Also Implement SSR, SSG all the projects.`,
   },
-  {
-    id: 2,
-    position: "Junior Developer",
-    company: "IRD Foundation, Banasree, Dhaka",
-    duration: "2022 - Present",
-    details:
-      "Configure & deploy web applications based on the LEMP and LAMP stacks.Troubleshoot various production environment issues. Training new candidates on various system administration processes",
-  },
+  // {
+  //   id: 2,
+  //   position: "Junior Developer",
+  //   company: "IRD Foundation, Banasree, Dhaka",
+  //   duration: "2022 - Present",
+  //   details:
+  //     "Configure & deploy web applications based on the LEMP and LAMP stacks.Troubleshoot various production environment issues. Training new candidates on various system administration processes",
+  // },
 ];
