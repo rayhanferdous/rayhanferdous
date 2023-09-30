e909b779c996687576923b22e403031ce94e7f08
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-30 20:28:42 +0600

    Update ExperianceCard.jsx

--- components/Layout/Index/ExperianceCard.jsx
+++ components/Layout/Index/ExperianceCard.jsx
@@ -3,13 +3,15 @@ import React from "react";
 
 const ExperianceCard = ({ position, company, duration, details }) => {
   return (
-    <div className="p-5 border border-solid border-primary-border rounded-[10px]">
-      <div className="flex gap-4">
-        <BulletPoint />
-        <p className="text-h6 text-lg font-semibold">{position}</p>
+    <div className="p-5 border border-solid flex flex-col gap-5 border-primary-border rounded-[10px]">
+      <div>
+        <div className="flex gap-4">
+          <BulletPoint />
+          <p className="text-h6 text-lg font-semibold">{position}</p>
+        </div>
+        <p className="text-primary-border">{company}</p>
+        <p className="text-primary-border">{duration}</p>
       </div>
-      <p className="text-primary-border">{company}</p>
-      <p className="text-primary-border">{duration}</p>
       <hr className="border-primary-border" />
       <p className="text-primary-border">{details}</p>
     </div>
