830aa1779cbbe00e1bc998af3937a225cd19095f
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-30 20:19:25 +0600

    Update Rectangle.js

--- components/Layout/Rectangle.js
+++ components/Layout/Rectangle.js
@@ -3,7 +3,7 @@ import React from "react";
 function Rectangle({ className }) {
   return (
     <div
-      className={`${className} w-702 h-702 bg-btnPrimary blur-240 -z-[1] absolute opacity-70`}
+      className={`${className} max-md:hidden w-702 h-702 bg-btnPrimary blur-240 -z-[1] absolute opacity-70`}
     ></div>
   );
 }
