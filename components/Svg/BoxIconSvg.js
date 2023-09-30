4c024e19c1ed19fb2c3550262f5218c42e78bd3d
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-09-30 21:10:46 +0600

    Update BoxIconSvg.js

--- components/Svg/BoxIconSvg.js
+++ components/Svg/BoxIconSvg.js
@@ -12,37 +12,37 @@ export function BoxIconSvg() {
       <path
         d="M12.5 2.5L22.5 9V16L12.5 22.5L2.5 16V9L12.5 2.5Z"
         stroke="#7389AA"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12.5 22.5V16"
         stroke="#7389AA"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M22.5 9L12.5 16L2.5 9"
         stroke="#7389AA"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M2.5 16L12.5 9L22.5 16"
         stroke="#7389AA"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12.5 2.5V9"
         stroke="#7389AA"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
