32934161c97ec43cd58e7364fcc901643e7edd3c
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2024-07-05 14:49:32 +0600

    Update ExperianceData.js

--- data/ExperianceData.js
+++ data/ExperianceData.js
@@ -3,7 +3,7 @@ export const experianceData = [
     id: 1,
     position: "Junior Developer",
     company: "EcomClips Ltd.",
-    duration: "Nov, 2023 - Present",
+    duration: "Nov, 2023 - Apr. 2024",
     details:
       "● Built responsive websites using semantic HTML5, PHP, Laravel, JavaScript, ReactJS, NextJs,and CSS, Tailwind CSS, Redux, and other tools. ● Connected API with frontend. ● Developed code that deals with large data sets by rendering components on UI and optimizing calls to minimize HTTP requests by 80%. Also Implement SSR, SSG all the projects. ● Deploy code in Cpanel, etc. ● Worked with Git, Github, etc. ● Worked with Jira.",
   },
